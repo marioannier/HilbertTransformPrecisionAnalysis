bc70b406727bc2de26fcc697f3791db7e2663167
marioannier <57014126+marioannier@users.noreply.github.com>
2023-12-15 18:12:35 +0100

    some changes

--- FHT_ErrorAnalyzer.py
+++ FHT_ErrorAnalyzer.py
@@ -10,11 +10,11 @@ class HilbertTransformErrorAnalyzer:
 
       Attributes:
           order (float): order ρ of the FHT results in a phase shift of ±ρπ/2 around the central frequency
-          number_coef (int): NUmber of coefficients for bulting the FHT.
+          number_coef (int): Number of coefficients for building the FHT.
 
       Methods:
           __init__(self, order, number_coef):
-              Initializes the HilbertTransformErrorAnalyzer for an order using a number_coef.
+               Initialize the HilbertTransformErrorAnalyzer for an order using a number_coef.
 
           calculate_error(self):
               Calculates the error in the fractional Hilbert transform based on the provided coefficients
@@ -35,7 +35,7 @@ class HilbertTransformErrorAnalyzer:
           >>> print(error)
       """
 
-    def __init__(self, num_sim=1000, err_range=[-0.1, 0.1], order=1, number_coef=7, f_center=8e9):
+    def __init__(self, num_sim=1000, err_range=[-10, 10], order=1, number_coef=7, f_center=8e9):
         # Constructor implementation
         self.num_sim = num_sim
         self.err_range = err_range
@@ -69,7 +69,6 @@ class HilbertTransformErrorAnalyzer:
 
         for c in range(len(ak)):
             response += ak[c] * np.exp(-1j * tk[c] * 2 * np.pi * f) * np.exp(1j * 5.5 * T * 2 * np.pi * f)
-
         return response
 
     def calculate_rel_rmse(self, theory_data, measured_data):
@@ -87,15 +86,12 @@ class HilbertTransformErrorAnalyzer:
         rmse = np.sqrt(np.mean((theory_data - measured_data) ** 2))
 
         # Calculate data range
-        data_range = np.max(theory_data) - np.min(theory_data)
-        # Alternatively, you can use the data range of measured_data:
-        # data_range = np.max(measured_data) - np.min(measured_data)
+        data_range = np.max(theory_data) - np.min(measured_data)
 
         # Calculate relative RMSE
         rel_rmse = (rmse / data_range) * 100
         # Alternatively, you can use absolute RMSE without normalization:
         # rel_rmse = rmse
-
         return rel_rmse
 
     def calculate_coef(self, angle=90, numtaps=7):
@@ -124,8 +120,6 @@ class HilbertTransformErrorAnalyzer:
         for i in range(1, numtaps // 2 + 1):
             td_index = np.argmax(t[len(t) // 2:] >= 2 * (i - 1) + 1)
             coef_pos[i - 1] = fht[len(t) // 2 + td_index]
-        # remove zero coefficients
-        # coef_pos = coef_pos[coef_pos != 0]
 
         # the central coefficient zero for classical HT
         coef_zeroth = np.array(round(abs(fht[len(t) // 2]), 3))
@@ -157,7 +151,7 @@ class HilbertTransformErrorAnalyzer:
 
         return ak, tk
 
-    def error_calculation(self, num_sim=100, err_range=[-0.02, 0.02], order=1, number_coef=7, f_center=8e9): # 0.02 is 10% of 2T
+    def error_calculation(self, num_sim=100, err_range=[-10, 10], order=1, number_coef=7, f_center=8e9):
 
         NRMSE = np.zeros((2, num_sim, num_sim))
         angle = np.rad2deg(order * np.pi / 2)
@@ -169,6 +163,8 @@ class HilbertTransformErrorAnalyzer:
         T = 1 / FSR
         f = np.linspace(0e9, 20e9, 3001)
 
+        # transforming err range percentage to number
+        err_range = np.array(err_range) * 0.01
         # the error in time is based on T, the error goes from err_range[0]*T to err_range[0]*T with num_sim steps
         error_limits = np.linspace(err_range[0], err_range[1], num_sim)
 
@@ -185,12 +181,11 @@ class HilbertTransformErrorAnalyzer:
         response_non_ref_phase = np.angle(response_non_ref[300:2200])  # the bandwidth of interest is delimited here
 
         for n in range(num_sim):
-            print('Simulating: ', n, ' of: ', num_sim)
+            print('Simulating: ', n, ' out of ', num_sim , ' simulations')
             for i, e in enumerate(error_limits):
                 # adding the error
                 tk_rand = np.random.rand(number_coef) * e
-                tk_2 = tk + tk_rand - tk[
-                    0]  # - tk[0] is used because the response_nonuni_HT() works only with positive values
+                tk_2 = tk + tk_rand - tk[0]  # - tk[0] is used because the response_nonuni_HT() works only with positive values
 
                 response_non = -self.response_nonuni_HT(f, ak, T * tk_2, T)
                 response_non_dB = 10 * np.log10(np.abs(response_non[300:2200]))
@@ -203,78 +198,42 @@ class HilbertTransformErrorAnalyzer:
 
         return NRMSE
 
-    def center_values_3d_histogram(self, data):
-        # Ensure data is a 3D array
-        if data.ndim != 3:
-            raise ValueError("Input array must be 3D")
-
-        # Get the shape of the array
-        dim1, dim2, dim3 = data.shape
-
-        # Initialize an array to store center values
-        center_values = np.zeros((dim1, dim2))
-
-        # Define a Gaussian function for curve fitting
-        def gaussian(x, amplitude, mean, stddev):
-            return amplitude * np.exp(-((x - mean) / (2 * stddev)) ** 2)
-
-        # Calculate histograms and find center value for each slice along the third dimension
-        for i in range(dim1):
-            for j in range(dim2):
-                histogram, bin_edges = np.histogram(data[i, j, :], bins=20)
-                bin_centers = (bin_edges[:-1] + bin_edges[1:]) / 2
-
-                # Fit the histogram to a Gaussian function
-                initial_guess = [np.max(histogram), bin_centers[np.argmax(histogram)], np.std(data[i, j, :])]
-                params, covariance = curve_fit(gaussian, bin_centers, histogram, p0=initial_guess)
-                print(params)
-
-                # Extract the mean (center) value from the fitted parameters
-                center_values[i, j] = params[1]
-                '''
-
-                # Plot the histogram and the fitted Gaussian
-                plt.figure()
-                plt.plot(bin_centers, histogram, label='Histogram')
-                plt.plot(bin_centers, gaussian(bin_centers, *params), label='Gaussian Fit')
-                plt.title(f'Histogram and Gaussian Fit for i={i}, j={j}')
-                plt.xlabel('Value')
-                plt.ylabel('Frequency')
-                plt.legend()
-                plt.show()'''
-
-        return center_values
-
 
     def plot_nrmse(self, NRMSE, err_range, num_sim):
 
-        mpl.use('TkAgg')  # or can use 'TkAgg', whatever you have/prefer
+        mpl.use('TkAgg')  # using 'TkAgg' to handling figures
 
         porc = np.linspace(err_range[0], err_range[1], num_sim)
-        #NRMSE_2d = np.mean(NRMSE, axis=2)
-        NRMSE_2d = self.center_values_3d_histogram(NRMSE)
+        NRMSE_2d = np.mean(NRMSE, axis=2)
+        # getting two random simulations
+        rd = np.random.randint(2, num_sim , size=2)
 
         # Plot for the first case
         plt.figure()
-        plt.plot(porc, NRMSE[0, :, 0])
+        plt.plot(porc, NRMSE[0, :, rd[0]])
+        plt.plot(porc, NRMSE[0, :, rd[1]])
         plt.plot(porc, NRMSE_2d[0, :], '--k', linewidth=1.5)
-        plt.legend(['RMSE Case 1', '', 'Mean RMSE (5000 cases)'])
-        plt.xlabel('Taps error rate (%)')
-        plt.ylabel('RMSE')
+        plt.legend(['N-RMSE random Case N°1', 'N-RMSE random Case N°2', 'Mean N-RMSE'])
+        plt.xlabel('Maximum error rate in all Taps (%)')
+        plt.ylabel('N-RMSE(%)')
+        plt.title('Magnitude N-RMSE distribution')
 
         # Plot for the second case
         plt.figure()
-        plt.plot(porc, NRMSE[1, :, 0])
+        plt.plot(porc, NRMSE[1, :, rd[0]])
+        plt.plot(porc, NRMSE[1, :, rd[1]])
         plt.plot(porc, NRMSE_2d[1, :], '--k', linewidth=1.5)
-        plt.legend(['RMSE Case 1', '', 'Mean RMSE (5000 cases)'])
-        plt.xlabel('Taps error rate (%)')
-        plt.ylabel('RMSE')
-        plt.show()
+        plt.legend(['N-RMSE random Case N°1', 'N-RMSE random Case N°2', 'Mean N-RMSE'])
+        plt.xlabel('Maximum error rate in all Taps (%)')
+        plt.ylabel('N-RMSE(%)')
+        plt.title('Phase N-RMSE distribution')
+
 
     def plot_3d_nrmse(self, NRMSE, err_range, num_sim):
-        mpl.use('TkAgg')  # or can use 'TkAgg', whatever you have/prefer
+        mpl.use('TkAgg')  # using 'TkAgg' to handling figures
         # Get the dimensions
         dim1, dim2, dim3 = NRMSE.shape
+        # arrgar la correpondencia con los ejes
 
         # Plot for the first dimension
         fig1 = plt.figure(figsize=(8, 6))
@@ -284,18 +243,17 @@ class HilbertTransformErrorAnalyzer:
         ax1.set_title('First Dimension')
         ax1.set_xlabel('X-axis')
         ax1.set_ylabel('Y-axis')
-        ax1.set_zlabel('Value')
+        ax1.set_zlabel('3D Magnitude N-RMSE distribution')
 
         # Plot for the second dimension
         fig2 = plt.figure(figsize=(8, 6))
         ax2 = fig2.add_subplot(111, projection='3d')
         x2, y2 = np.meshgrid(np.linspace(err_range[0], err_range[1], num_sim), range(dim3))
         ax2.plot_surface(x2, y2, NRMSE[1, :, :], cmap='viridis')
-        ax2.set_title('Second Dimension')
+        ax2.set_title('3D Phase N-RMSE distribution')
         ax2.set_xlabel('X-axis')
         ax2.set_ylabel('Y-axis')
         ax2.set_zlabel('Value')
 
-        plt.show()
 
 
--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 from FHT_ErrorAnalyzer import HilbertTransformErrorAnalyzer
 
 My_FHTErrorAnalyzer = HilbertTransformErrorAnalyzer()
-err_range = [-0.025, 0.025]
-num_sim = 100
+err_range = [-50, 50]
+num_sim = 101
 order = 1
 number_coef = 7
 f_center = 8e9
 NRMSE = My_FHTErrorAnalyzer.error_calculation(num_sim, err_range, order, number_coef, f_center)
 My_FHTErrorAnalyzer.plot_nrmse(NRMSE, err_range, num_sim)
-#My_FHTErrorAnalyzer.plot_3d_nrmse(NRMSE, err_range, num_sim)
+My_FHTErrorAnalyzer.plot_3d_nrmse(NRMSE, err_range, num_sim)
+plt.show()
